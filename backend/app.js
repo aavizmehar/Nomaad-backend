18c57efa89c989ca487bfba49ef7e901acf6c4a0
aaviz mehar <aavizk526@gmail.com>
2025-12-26 22:42:40 +0530

    fix

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.use(passport.initialize());
 // routes
 const userRouter = require('./routes/user.routes.js');
 const hostRouter = require('./routes/host.routes.js');
